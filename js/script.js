4a787affccd80d0fb104b762a7049ff5f4424f89
andrevisualstudio <andrevisualstudio@gmail.com>
2026-07-13 13:06:13 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+console.log("Andre Visual Studio");
 
+window.addEventListener("scroll",function(){
+
+const nav=document.querySelector("nav");
+
+if(window.scrollY>80){
+
+nav.style.background="#000";
+
+}else{
+
+nav.style.background="rgba(0,0,0,.7)";
+
+}
+
+});
